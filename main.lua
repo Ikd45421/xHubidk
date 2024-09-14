2cc682683f0a476ac12b9255af1100cd07e08e80
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-14 21:14:53 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local workspace = game:GetService("Workspace")
 local players = game:GetService("Players")
+local lighting = game:GetService("Lighting")
 
 local camera = workspace.Camera
 
@@ -40,12 +41,36 @@ local Settings = {
 }
 
 Player.Movement:AddSlider(0, {
-	Text = "Aaaa",
+	Text = "Walk Speed",
 	Default = 16,
 	Min = 0,
-	Max = 150,
+	Max = 100,
 	Rounding = 0,
 	Callback = function(value) humanoid.WalkSpeed = value end
+})
+
+Visual.Lighting:AddToggle(0, {
+	Text = "Fullbright",
+	Default = false,
+	Risky = false,
+	Callback = function(value)
+		if value then
+			lighting.Brightness = 25
+			lighting.Ambient = Color3.fromRGB(255, 255, 255)
+		else
+			lighting.Brightness = 2
+			lighting.Ambient = Color3.fromRGB(70, 70, 70)
+		end
+	end
+})
+
+Visual.Camera:AddSlider(2, {
+	Text = "Field of View",
+	Default = 70,
+	Min = 30,
+	Max = 120,
+	Rounding = 0,
+	Callback = function(value) camera.FieldOfView = value end
 })
 
 Settings.Config:AddButton("Unload", function() Library:Unload() end)
