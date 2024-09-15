9aea1a0d1b0aa7ea26dbb8e8727f8015ff52a3da
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 20:50:15 +0100

    Update MovementManager.lua

--- utils/MovementManager.lua
+++ utils/MovementManager.lua
@@ -1,7 +1,9 @@
 local players = game:GetService("Players")
+local workspace = game:GetService("Workspace")
 local player = players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character.Humanoid
+local camera = workspace.Camera
 
 local options = getgenv().Linoria.Options
 
@@ -12,5 +14,15 @@ humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
         if humanoid.WalkSpeed == 16 + speedBoost then return end
 
         humanoid.WalkSpeed = 16 + speedBoost
+    end
+)
+
+camera:GetPropertyChangedSignal("FieldOfView"):Connect(function()
+        local fov = options[1].Value
+
+        if not fov then return end
+        if camera.FieldOfView == fov then return end
+
+        camera.FieldOfView = fov
     end
 )
