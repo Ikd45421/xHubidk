109fc7142c553dbedd7a57ab43917d557df8a9a6
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 02:22:17 +0100

    Update Lighting.lua

--- Lighting.lua
+++ Lighting.lua
@@ -1,6 +1,8 @@
 local lighting = game:GetService("Lighting")
 
-function SetFullbright(enabled)
+local Lighting = {}
+
+function Lighting:SetFullbright(enabled)
   if enabled then
     lighting.Brightness = 25
     lighting.Ambient = Color3.fromRGB(255, 255, 255)
@@ -9,3 +11,5 @@ function SetFullbright(enabled)
     lighting.Ambient = Color3.fromRGB(40, 53, 65)
   end
 end
+
+return Lighting
