e132d3ac3eee2b890b13417635025da14c353fcc
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 19:32:59 +0100

    Update InteractionManager.lua

--- utils/InteractionManager.lua
+++ utils/InteractionManager.lua
@@ -1,7 +1,8 @@
 local proximityPromptService = game:GetService("ProximityPromptService")
+local toggles = getgenv().Linoria.Toggles
 
 proximityPromptService.PromptButtonHoldBegan:Connect(function(prompt)
-        if not getgenv().instantInteract.Value then return end
+        if not toggles[0].Value then return end
 
         prompt.HoldDuration = 0
     end
