4f8f2ae334dfa5231168ff60b048703dd283d613
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 11:13:54 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,6 @@ local inGame = game.PlaceId == 12411473842
 
 local places = "https://raw.githubusercontent.com/xBackpack/PressureHub/main/places/"
 
-if inGame then loadstring(game:HttpGet(places .. game.PlaceId .. ".lua"))() end
+print("D")
+
+if inGame then loadstring(game:HttpGet(places .. game.PlaceId .. ".lua"))(); print("E") end
