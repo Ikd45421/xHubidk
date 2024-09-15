96117444179c67768d79b5118e63d4975b69ea67
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 10:55:44 +0100

    Update Lighting.lua

--- Lighting.lua
+++ Lighting.lua
@@ -4,10 +4,8 @@ local module = {}
 
 function module:SetFullbright(enabled)
   if enabled then
-    lighting.Brightness = 25
     lighting.Ambient = Color3.fromRGB(255, 255, 255)
   else
-    lighting.Brightness = 3
     lighting.Ambient = Color3.fromRGB(40, 53, 65)
   end
 end
