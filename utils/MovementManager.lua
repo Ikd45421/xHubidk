e1ed91417872c277f97ac50238dc81be853c0814
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 17:27:34 +0100

    Update MovementManager.lua

--- utils/MovementManager.lua
+++ utils/MovementManager.lua
@@ -3,9 +3,12 @@ local player = players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character.Humanoid
 
-player.Crouching.Changed:Connect(function(newValue)
-        if getgenv().speedBoost.Value == 0 then return end
+humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+        local speedBoost = getgenv().speedBoost.Value
 
-        humanoid.WalkSpeed = 16 + getgenv().speedBoost.Value
+        if speedBoost == 0 then return end
+        if humanoid.WalkSpeed == 16 + speedBoost then return end
+
+        humanoid.WalkSpeed = 16 + speedBoost
     end
 )
