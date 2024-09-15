9a9c97f54d6e2b937869c1b07d2eddc87c4b925d
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 13:59:19 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xBackpack/PressureHub/main/main.lua"))()
 
 local validPlaceIds = {12552538292, }
+local foundGame = false
 
 local linoriaLib = "https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/"
 local addons = linoriaLib .. "addons/"
@@ -11,9 +12,15 @@ local saveManager = loadstring(game:HttpGet(addons .. 'SaveManager.lua'))()
 
 local placesRepo = "https://raw.githubusercontent.com/xBackpack/PressureHub/main/places/"
 
-for _, id in pairs(validPlaceIds) do
+for _, id in ipairs(validPlaceIds) do
   if game.PlaceId == id then
     loadstring(game:HttpGet(placesRepo .. game.PlaceId .. ".lua"))()
+    foundGame = true
     break
   end
+end
+
+if not foundGame then
+  library:Notify("The place you are currently in is not valid. Please look at our github for a list of valid games!")
+  library:Unload()
 end
