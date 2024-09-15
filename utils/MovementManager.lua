815e27bc63b2a47df6b133cf5b1c5d80932e9f9e
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 19:33:33 +0100

    Update MovementManager.lua

--- utils/MovementManager.lua
+++ utils/MovementManager.lua
@@ -3,8 +3,10 @@ local player = players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character.Humanoid
 
+local options = getgenv().Linoria.Options
+
 humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
-        local speedBoost = getgenv().speedBoost.Value
+        local speedBoost = options[0].Value
 
         if speedBoost == 0 then return end
         if humanoid.WalkSpeed == 16 + speedBoost then return end
