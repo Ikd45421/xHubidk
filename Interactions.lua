0e29f9effa93e035230f169280160bcd57e15343
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-15 02:19:46 +0100

    Update Interactions.lua

--- Interactions.lua
+++ Interactions.lua
@@ -1,3 +1,14 @@
-function SetInstantInteract(enabled)
+local pps = game:GetService("ProximityPromptService")
 
+local enabled = false
+
+function SetInstantInteract(value)
+  enabled = value
 end
+
+pps.PromptButtonHoldBegan:Connect(function(prompt, player)
+    if not enabled then return end
+
+    prompt.HoldDuration = 0
+  end
+)
