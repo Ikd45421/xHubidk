a83245544f851b50ccc6e45580934995011108ae
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-16 16:09:09 +0100

    Update InteractionManager.lua

--- utils/InteractionManager.lua
+++ utils/InteractionManager.lua
@@ -4,7 +4,7 @@ local toggles = getgenv().Linoria.Toggles
 proximityPromptService.PromptButtonHoldBegan:Connect(function(prompt)
         if not getgenv().PressureHubLoaded then return end
         
-        if not toggles[0].Value then return end
+        if not toggles.InstantInteract.Value then return end
 
         prompt.HoldDuration = 0
     end
