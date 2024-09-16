337eafc6fa1c4aa95d4cbcee945789d395623eb9
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-16 16:03:28 +0100

    Update FieldOfViewManager.lua

--- utils/FieldOfViewManager.lua
+++ utils/FieldOfViewManager.lua
@@ -4,7 +4,7 @@ local camera = workspace.Camera
 camera:GetPropertyChangedSignal("FieldOfView"):Connect(function()
         local fov = options[1].Value
 
-        if not fov then return end
+        if fov == 90 then return end
         if camera.FieldOfView == fov then return end
 
         camera.FieldOfView = fov
