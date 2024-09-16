77d2bd70d8b2e227c45c5750ad6d2b26c4de301e
xBackpack <112735017+xBackpack@users.noreply.github.com>
2024-09-16 16:08:38 +0100

    Update FieldOfViewManager.lua

--- utils/FieldOfViewManager.lua
+++ utils/FieldOfViewManager.lua
@@ -4,7 +4,7 @@ local camera = workspace.Camera
 camera:GetPropertyChangedSignal("FieldOfView"):Connect(function()
         if not getgenv().PressureHubLoaded then return end
         
-        local fov = options[1].Value
+        local fov = options.Fullbright.Value
 
         if fov == 90 then return end
         if camera.FieldOfView == fov then return end
