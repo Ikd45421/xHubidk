ff158458408d939837503bfa86a4e35536c47939
xBackpack <jamescheeseman21@gmail.com>
2024-09-29 20:51:26 +0100

    added spy

---
+++ places/spy.lua
@@ -0,0 +1,14 @@
+local zoneChangeEvent = game.ReplicatedStorage.Events.ZoneChange
+
+local oldFireServer
+oldFireServer = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod()
+
+    if method == "FireServer" and self == zoneChangeEvent then
+        local args = {...}
+        local room = args[1]
+        print("Zone Change Called: " .. room.Name)
+    end
+
+    return oldFireServer(self, ...)
+end)
